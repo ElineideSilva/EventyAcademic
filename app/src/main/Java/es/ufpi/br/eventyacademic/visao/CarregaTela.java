3a444fb985125dd9a0f1c96abfec7a55ad4c356b
laftel <rafahmmds@gmail.com>
2017-11-11 17:50:28 -0300

    Tentando arrumar bug de repositorio

---
+++ app/src/main/Java/es/ufpi/br/eventyacademic/visao/CarregaTela.java
@@ -0,0 +1,13 @@
+package es.ufpi.br.eventyacademic.visao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarregaTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrega_tela);
+    }
+}
---
+++ app/src/main/Java/es/ufpi/br/eventyacademic/visao/TelaConsultarEvento.java
@@ -0,0 +1,13 @@
+package es.ufpi.br.eventyacademic.visao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaConsultarEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_consultar_evento);
+    }
+}
