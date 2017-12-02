38bc5de71ba42db88ae40a395ed9d2ccd78c1ba9
Thiago Andrade <thiagoborginho@hotmail.com>
2017-12-02 18:48:12 -0200

    finalizando as classes do banco de dados

--- app/src/main/Java/br/com/ufpi/es/eventyacademic/dados/banco_de_dados/Database.java
+++ app/src/main/Java/br/com/ufpi/es/eventyacademic/dados/banco_de_dados/Database.java
@@ -1,7 +1,10 @@
 package br.com.ufpi.es.eventyacademic.dados.banco_de_dados;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.*;
+
 
 /**
  * Created by Thanbo on 02/12/2017.
@@ -11,20 +14,44 @@ import android.database.sqlite.*;
 public class Database extends SQLiteOpenHelper {
     //criação do banco
 
+    SQLiteDatabase db;
+
     public Database(Context context){
         super(context,"Eventos",null,1);
-
+        db = this.getReadableDatabase();
 
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL(ScriptSql.criarEvento());
+        db.execSQL(ScriptSql.criarWorkshop());
+        db.execSQL(ScriptSql.criarUsuario());
+        db.execSQL(ScriptSql.criarPalestra());
+        db.execSQL(ScriptSql.criarMinicurso());
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        db.execSQL(ScriptSql.deletaEvento());
+        onCreate(db);
     }
+
+    public Cursor recuperarUsuario (String tabela){
+        Cursor retorno = db.rawQuery("SELECT * FROM " + tabela, null);
+        return retorno;
+    }
+
+    public void inserirUsuario(ContentValues values, String tabela){
+        if(values.containsKey("USERNAME") && values.getAsString("USERNAME")!= null){
+            String username = values.getAsString("username");
+            db.update(tabela,values,"username= " +username,null);
+        }
+        else{
+            db.insert(tabela,null,values);
+        }
+    }
+
 }
--- app/src/main/Java/br/com/ufpi/es/eventyacademic/dados/banco_de_dados/ScriptSql.java
+++ app/src/main/Java/br/com/ufpi/es/eventyacademic/dados/banco_de_dados/ScriptSql.java
@@ -1,5 +1,7 @@
 package br.com.ufpi.es.eventyacademic.dados.banco_de_dados;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 /**
  * Created by Thanbo on 02/12/2017.
  */
@@ -13,10 +15,128 @@ public class ScriptSql {
 
     public static String criarEvento (){
 
+
+            String query = "CREATE TABLE EVENTO";
+            query+= " (";
+            query+= "NOME TEXT,";
+            query+= "LOCAL TEXT,";
+            query+= "TIPO INT,";
+            query+= "DATA_I TEXT,";
+            query+= "DATA_F TEXT";
+            query+= ")";
+
+            return query;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static String criarUsuario (){
+
+
+        String query = "CREATE TABLE USUARIO";
+        query+= " (";
+        query+= "NOME TEXT,";
+        query+= "CPF INT,";
+        query+= "TIPO INT,";
+        query+= "EMAIL TEXT,";
+        query+= "SENHA TEXT";
+        query+= "USERNAME TEXT";
+        query+= "DATA_N TEXT";
+        query+= ")";
+
+        return query;
+    }
+
+    /**
+     *
+     * @return
+     */
+
+    public static String criarPalestra (){
+
+
+        String query = "CREATE TABLE PALESTRA";
+        query+= " (";
+        query+= "NOME TEXT,";
+        query+= "DATA_I TEXT,";
+        query+= "DATA_F TEXT,";
+        query+= ")";
+
+        return query;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static String criarWorkshop (){
+
+
+        String query = "CREATE TABLE WORKSHOP";
+        query+= " (";
+        query+= "NOME TEXT,";
+        query+= "DATA_I TEXT,";
+        query+= "DATA_F TEXT,";
+        query+= ")";
+
+        return query;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static String criarMinicurso (){
+
+
+        String query = "CREATE TABLE MINICURSO";
+        query+= " (";
+        query+= "NOME TEXT,";
+        query+= " DATA_I TEXT,";
+        query+= " DATA_F TEXT,";
+        query+= ")";
+
+        return query;
+    }
+
+    public static String criarUserEvent (){
+
+
+        String query = "CREATE TABLE USUARIOEVENTO";
+        query+= " (";
+        query+= "NOMEUSUARIO TEXT,";
+        query+= "NOMEEVNT TEXT,";
+        query+= ")";
+
+        return query;
+    }
+
+    public static String criarEventUser (){
+
+
+        String query = "CREATE TABLE WORKSHOP";
+        query+= " (";
+        query+= "NOMEEVENT TEXT,";
+        query+= "NOMEUSER TEXT,";
+        query+= ")";
+
+        return query;
+    }
+
+    /**
+     * cria a string de deletar o banco
+     * @return a string montada
+     */
+    public static String deletaEvento (){
+
         StringBuilder sqlBuilder = new StringBuilder();
-        sqlBuilder.append("scriptcom espaço no fim de cada  chamada ");
+        sqlBuilder.append("DELETE TABLE");
 
         return sqlBuilder.toString();
     }
+
+
 
 }
