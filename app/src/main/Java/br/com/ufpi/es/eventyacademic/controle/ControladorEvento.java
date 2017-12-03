2302061913cc0c687a5a6caf6b66bf4f0962561b
peterd130 <peterdias130@gmail.com>
2017-12-03 13:06:16 -0200

    implementação de cadastrar em evento

--- app/src/main/Java/br/com/ufpi/es/eventyacademic/controle/ControladorEvento.java
+++ app/src/main/Java/br/com/ufpi/es/eventyacademic/controle/ControladorEvento.java
@@ -1,6 +1,7 @@
 package br.com.ufpi.es.eventyacademic.controle;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Date;
 
 import br.com.ufpi.es.eventyacademic.dados.Evento;
@@ -10,6 +11,7 @@ import br.com.ufpi.es.eventyacademic.dados.*;
 
 public class ControladorEvento {
     RepositorioEventos r = new RepositorioEventos();
+    RepositorioUsuarios ru = new RepositorioUsuarios();
     Evento e;
 
     /**
@@ -18,10 +20,10 @@ public class ControladorEvento {
      */
 
     public boolean cadastraEvento(String nome, String local, int tipo, Date data_inicio,
-                                  Date data_fim, Usuario organizador, ArrayList<Usuario> participantes){
+                                  Date data_fim, Usuario organizador){
 
         if(r.buscar(nome) == null){
-            e = new Evento(nome, local, tipo, data_inicio, data_fim, organizador, participantes);
+            e = new Evento(nome, local, tipo, data_inicio, data_fim, organizador);
             r.inserir(e);
         }else{
             return false;
@@ -29,9 +31,35 @@ public class ControladorEvento {
         return true;
     }
 
-    public Evento buscarEvento(String nome){
+    public Evento consultarEvento(String nome){
 
         return r.buscar(nome);
 
     }
+    
+    public List<Evento> verTodosEventos(){
+    	
+    	return r.listar();
+    	
+    }
+
+    /**
+     * Recebe o email do usuario que quer se inscrever e o nome do evento.
+     * Verifica se o mesmo já está inscrito no evento, em caso positivo retorna false,
+     * em caso negativo adiciona na lista de participantes do evento e retorna true
+     */
+
+    public boolean increverNoEvento(String email, String nomedoevento) {
+    	
+    	Evento e = r.buscar(nomedoevento);
+    	Usuario u = ru.buscar(email);
+    	
+    	if(e.getParticipantes().contains(u))
+    		return false;
+    	
+    	e.setParticipantes(u);
+    	return true;
+    	
+    }
+    
 }
