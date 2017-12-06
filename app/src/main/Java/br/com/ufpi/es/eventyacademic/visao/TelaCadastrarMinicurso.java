951840c031b47f35aecf87988eccb4086349e405
Segundo <josevitorsegundo@hotmail.com>
2017-12-05 23:20:11 -0300

    Arquivos de novas tela criadas

---
+++ app/src/main/Java/br/com/ufpi/es/eventyacademic/visao/TelaCadastrarMinicurso.java
@@ -0,0 +1,15 @@
+package br.com.ufpi.es.eventyacademic.visao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.ufpi.es.eventyacademic.R;
+
+public class TelaCadastrarMinicurso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastrar_minicurso);
+    }
+}
